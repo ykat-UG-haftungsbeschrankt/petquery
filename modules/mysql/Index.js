147348c4ad8198d3777124b888e0593ba4782c95
matthiasrieger <162315008+matthiasrieger@users.noreply.github.com>
2024-08-13 12:37:23 +0200

    Update Index.js

--- modules/mysql/Index.js
+++ modules/mysql/Index.js
@@ -1,46 +1,48 @@
 const PetQueryModule = require('../../PetQueryModule.js');
 const mysql = require('mysql');
 
-class PetQueryModulesTest extends PetQueryModule{
-	constructor(cfg){
-            this._db = null;
-	    super(cfg);
+class PetQueryModulesMysql extends PetQueryModule {
+	constructor(cfg) {
+		this._db = null;
+		super(cfg);
 	}
-  async _getConnection(){
-    if(this._db === null){
-      this._db = mysql.createConnection(this.getConfigValue('db')/*{
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "mydb"
-}*/);
-	   await new Promise((resolve,reject)=>{
+	async _getConnection() {
+		if (this._db === null) {
+			this._db = mysql.createConnection(this.getConfigValue('db')
+				/*{
+				  host: "localhost",
+				  user: "yourusername",
+				  password: "yourpassword",
+				  database: "mydb"
+				}*/
+			);
+			await new Promise((resolve, reject) => {
+				this._db.connect(function(err) {
+					if (err) {
+						reject(err);
+					}
+					resolve();
+				});
+			});
+		}
 
-		 this._db.connect(function(err) {
-  if (err) {reject(err);}
-resolve();
-  
-});  
-	   }); 
-    }
-    
-    return this._db;
-  }
-	async _buildSqlQuery(query){}
-	async query(query){
+		return this._db;
+	}
+	async _buildSqlQuery(query) {}
+	async query(query) {
 		let ret = [];
 		let db = await this._getConnection();
-		let result = await new Promise((resolve,reject)=>{
+		let result = await new Promise((resolve, reject) => {
+			db.query(await this._buildSqlQuery(query), function(err, result, fields) {
+				if (err) {
+					reject(err);
+				}
+				resolve(result);
+			});
+		});
 
-		 db.query(await this._buildSqlQuery(query), function (err, result, fields) {
-  if (err) {reject(err);}
-resolve(result);
-		 });
-	   }); 
-		
 		return ret;
 	}
 };
 
-module.exports = PetQueryModulesTest;
-
+module.exports = PetQueryModulesMysql;
