9ef46e238a504fad478a5f14070cb3a7f06ff883
Matthias Rieger <matthias.rieger@rehkitz.eu>
2024-08-14 22:01:14 +0200

    bugfix

--- modules/animalid.by/index.js
+++ modules/animalid.by/index.js
@@ -5,11 +5,12 @@ class PetQueryModulesAnimalidBy extends PetQueryModule{
 		super(cfg);
 	}
 	async query(query){
-		let ret = [];
-		let query = String(query.query).replace(/[^0-9]/g,'');
+		query = String(query.query).replace(/[^0-9]/g,'');
 		if(query == ''){
 			return [];
 		}
+
+		let ret = [];
 		let data = await fetch("https://animalid.by/", {
 			"headers": {
 				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
